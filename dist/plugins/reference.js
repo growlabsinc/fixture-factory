8ee86b9d11bc4a189357c4d0d95fe16711a46e42
mpd <mpd@jesters-court.net>
2017-06-08 12:57:58 -0700

    rebuild

--- dist/plugins/reference.js
+++ dist/plugins/reference.js
@@ -1,44 +1,75 @@
 'use strict';
 
-var _ = require('lodash');
-var factory;
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
 
-function _transform(model, split) {
-  var models = factory.dataModels;
+var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
 
-  model.method = models[split[1]];
+var _lodash = require('lodash');
 
-  if (split.length === 2) {
-    if (model.reference && model.reference.properties) {
-      model.method = _.extend(model.method, model.reference.properties);
-    }
-  } else {
-    model.method = models[split[1]][split[2]];
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+var ReferencePlugin = function () {
+  function ReferencePlugin(fixtureFactory) {
+    var _this = this;
+
+    _classCallCheck(this, ReferencePlugin);
+
+    this.fixtureFactoryWillGenerateField = function (event) {
+      var model = event.model;
+
+      var isRef = (0, _lodash.isString)(model.method) && model.method.indexOf('model') === 0;
+      var split;
+
+      if (isRef) {
+        split = model.method.split('.');
+        if (split[0] === 'model') {
+          model = _this._transform(model, split);
+        }
+      }
+    };
+
+    this.fixtureFactory = fixtureFactory;
+    this.enable();
   }
 
-  return model;
-}
+  //internals, stuff
 
-function transform(event) {
-  var model = event.model;
-  var isRef = _.isString(model.method) && model.method.indexOf('model') === 0;
-  var split;
+  _createClass(ReferencePlugin, [{
+    key: '_transform',
+    value: function _transform(model, split) {
+      var models = this.fixtureFactory.dataModels;
 
-  if (isRef) {
-    split = model.method.split('.');
-    if (split[0] === 'model') {
-      model = _transform(model, split);
+
+      model.method = models[split[1]];
+
+      if (split.length === 2) {
+        if (model.reference && model.reference.properties) {
+          model.method = (0, _lodash.extend)(model.method, model.reference.properties);
+        }
+      } else {
+        model.method = models[split[1]][split[2]];
+      }
+
+      return model;
     }
-  }
-}
 
-module.exports.enable = function (fixtureFactory) {
-  factory = fixtureFactory;
+    //api
 
-  fixtureFactory.on('field:pre', transform);
-};
+  }, {
+    key: 'enable',
+    value: function enable() {
+      this.fixtureFactory.on('field:pre', this.fixtureFactoryWillGenerateField);
+    }
+  }, {
+    key: 'disable',
+    value: function disable() {
+      this.fixtureFactory.removeListener('field:pre', this.fixtureFactoryWillGenerateField);
+    }
+  }]);
 
-module.exports.disable = function (fixtureFactory) {
-  factory.removeListener('field:pre', transform);
-  factory = null;
-};
+  return ReferencePlugin;
+}();
+
+exports.default = ReferencePlugin;
